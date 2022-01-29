4c9d149e6d0ed5f7e4ec818b9a871cbf619a7903
Tr00d <guillaume.faas@live.fr>
2022-01-29 16:46:27 +0100

    Removing unused step

--- tests/BankKata.Test.Acceptance/Steps/AccountSteps.cs
+++ tests/BankKata.Test.Acceptance/Steps/AccountSteps.cs
@@ -35,17 +35,6 @@ namespace BankKata.Test.Acceptance.Steps
                 DateTime.ParseExact(date, AccountPrinter.dateFormat, CultureInfo.InvariantCulture));
         }
 
-        [Then(@"I should see all statements in reverse chronological order")]
-        public void ThenIShouldSeeAllStatementsInReverseChronologicalOrder()
-        {
-            StringBuilder builder = new StringBuilder();
-            builder.AppendLine("DATE | AMOUNT | BALANCE");
-            builder.AppendLine("20/01/2021 | -500 | 2500");
-            builder.AppendLine("15/01/2021 | 2000 | 3000");
-            builder.AppendLine("10/01/2021 | 1000 | 1000");
-            Assert.AreEqual(builder.ToString(), this.driver.GetConsoleOutput());
-        }
-
         [When(@"I prints the account statements")]
         public void WhenIPrintsTheAccountStatements()
         {
